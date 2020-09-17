8aea1eb3ea02ae5c0528186f6ee0938c62d58625
Christopher Blanchard <git@nf3.co>
2020-09-17 19:46:57 +0000

    chore(Tests): Reorganise test suites
    - Add tests for 4.5

--- uk-address-postcode-validation/wp/menu.php
+++ uk-address-postcode-validation/wp/menu.php
@@ -1,12 +1,17 @@
 <?php
-if ( class_exists( 'WC_Integration' ) && defined( 'WOOCOMMERCE_VERSION' ) && version_compare( WOOCOMMERCE_VERSION, '2.1-beta-1', '>=' ) ) {
-	// Register the integration.
-	add_filter( 'woocommerce_integrations', add_integration );
+if (
+  class_exists('WC_Integration') &&
+  defined('WOOCOMMERCE_VERSION') &&
+  version_compare(WOOCOMMERCE_VERSION, '2.1-beta-1', '>=')
+) {
+  // Register the integration.
+  add_filter('woocommerce_integrations', add_integration);
 } else {
-	add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+  add_action('admin_notices', [$this, 'woocommerce_missing_notice']);
 }
 
-function add_integration(){
-	$integrations[] = 'WC_IdealPostcodes';
-	return $integrations;
+function add_integration()
+{
+  $integrations[] = 'WC_IdealPostcodes';
+  return $integrations;
 }
