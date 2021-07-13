d3ced268d5e13391bdc3173b02f8f1f9b7b984ff
mfilip <marcin.filip@gmail.com>
2021-07-13 09:45:20 +0200

    update

--- cypress/integration/007_admin_order.ts
+++ cypress/integration/007_admin_order.ts
@@ -24,7 +24,7 @@ const billingTest = () => {
       .focus()
       .type(address.line_1);
     //here wait because it not catching the xhr call to get list
-    cy.wait(1000);
+    cy.wait(3000);
     cy.get(".idpc_ul li").first().click({ force: true });
     cy.get(billingSelector.post_town).should(
       "have.value",
@@ -55,6 +55,7 @@ const shippingTest = () => {
     //here wait because it not catching the xhr call to get list
     cy.wait(1000);
     cy.get(".idpc_ul li").first().click({ force: true });
+    cy.wait(3000);
     cy.get(shippingSelector.post_town).should(
       "have.value",
       "Jersey"
